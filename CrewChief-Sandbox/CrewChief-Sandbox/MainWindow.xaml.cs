2cea5299a130aa9846b07d91353c8428106bfa25
Mike Profitt <mprofitt@ameritech.net>
2017-09-16 13:32:23 -0400

    Added intial coding

--- CrewChief-Sandbox/CrewChief-Sandbox/MainWindow.xaml.cs
+++ CrewChief-Sandbox/CrewChief-Sandbox/MainWindow.xaml.cs
@@ -12,9 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iRacingSdkWrapper;
 
 namespace CrewChief_Sandbox
 {
+    private readonly SdkWrapper wrapper;
+
+    private List<Weather> listWeather;
+    // Instantiate an instance of List class
+
+    private bool isUpdatingDrivers;
+
+
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +34,74 @@ namespace CrewChief_Sandbox
         public MainWindow()
         {
             InitializeComponent();
+
+            wrapper = new SdkWrapper();
+            wrapper.TelemetryUpdated += OnTelemetryUpdated;
+            wrapper.SessionInfoUpdated += OnSessionInfoUpdated;
+            wrapper.Connected += wrapper_Connected;
+            wrapper.Disconnected += wrapper_Disconnected;
+            wrapper.Start();
+        }
+        private void wrapper_Connected(object sender, EventArgs e)
+        {
+            Debug.WriteLine("*** wrapper connected ***\n");
+        }
+
+        private void wrapper_Disconnected(object sender, EventArgs e)
+        {
+            Debug.WriteLine("*** wrapper disconnected ***\n");
+        }
+
+        private void OnSessionInfoUpdated(object sender, SdkWrapper.SessionInfoUpdatedEventArgs e)
+        {
+            this.isUpdatingDrivers = true;
+
+            ParseWeather(e.SessionInfo);
+
+            this.isUpdatingDrivers = false;
+        }
+
+        private void OnTelemetryUpdated(object sender, SdkWrapper.TelemetryUpdatedEventArgs e)
+        {
+
+        }
+
+        private void ParseWeather(SessionInfo sessionInfo)
+        {
+            Weather weather = new Weather();
+            // Instantiate an instance of Weather class
+
+            var newWeather = new List<Weather>();
+            // Create a local list newWeather
+
+            YamlQuery query = sessionInfo["Weekendinfo"];
+            weather.SessionID = int.Parse(query["SessionID"].GetValue("0"));
+            weather.AirPressure = float.Parse(query["AirPressure"].GetValue("0"));
+            weather.Skies = query["Skies"].GetValue("0");
+            weather.AirTemp = float.Parse(query["AirTemp"].GetValue("0"));
+            weather.AirPressure = float.Parse(query["AirPressure"].GetValue("0"));
+            weather.WindVel = float.Parse(query["WindVel"].GetValue("0"));
+            weather.WindDir = float.Parse(query["WindDir"].GetValue("0"));
+            weather.RelativeHumidity = int.Parse(query["RelativeHumidity"].GetValue("0"));
+            weather.FogLevel = int.Parse(query["FogLevel"].GetValue("0"));
+
+            newWeather.Add(weather);
+            // Add data to list newWeather
+
+            // listWeather.Clear();
+            // Clear listWeather
+
+            // listWeather.AddRange(newWeather);
+            // Copy newWeather to listWeather
+
+            //weatherGrid.DataSource = weather;
+
+            // dataGridView1.DataSource = persons;
+
+
+
+            // var list = new BindingList<Weather>(weather);
+            //myGrid.DataSource = list;
         }
     }
 }
---
+++ CrewChief-Sandbox/CrewChief-Sandbox/Weather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewChief_Sandbox
+{
+    class Weather
+    {
+        public int SessionID { get; set; }
+        public string Skies { get; set; }
+
+        private float _airTemp;
+        public float AirTemp // Celcius
+        {
+            get
+            {
+                return _airTemp;
+            }
+            set
+            {
+                Math.Round(_airTemp = AirTemp * 1.8f + 32);
+            }
+        }
+
+        public float AirPressure { get; set; } // inches of mercury
+
+        private float _windVel;
+        public float WindVel  // meters / second
+        {
+            get
+            {
+                return _windVel;
+            }
+            set
+            {
+                _windVel = (float)Math.Round(WindVel * 2.27273f); // Convert mertes / second to MPH
+            }
+        }
+
+        public float WindDir { get; set; } // radians
+        public int RelativeHumidity { get; set; } // percentage
+        public int FogLevel { get; set; } // percentage
+    }
+}
+
+}
